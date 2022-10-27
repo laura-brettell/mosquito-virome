04e8c325da3199e9e7d0b130fd5f18414cb60ec9
Laura Brettell <38452231+laura-brettell@users.noreply.github.com>
2022-10-27 15:43:46 +0100

    Update extract_unmapped_bam.sh

--- scripts/extract_unmapped_bam.sh
+++ scripts/extract_unmapped_bam.sh
@@ -27,12 +27,3 @@ do
 done
 
 
-for sample in $(cat ./temp_sample_list.txt)
-do
-	bamToFastq \
-    -i ../unmapped_reads/"$sample"_both_ends_unmapped_sorted.bam  \
-    -fq ./unmapped_reads/"$sample"_host_removed_R1.fastq \
-    -fq2 ./unmapped_reads/ "$sample"_host_removed_R2.fastq
-    
-done
-
