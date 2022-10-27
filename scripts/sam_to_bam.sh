578334fd2c48e128013c59ac43c4c77eff9607f8
Laura Brettell <38452231+laura-brettell@users.noreply.github.com>
2022-10-27 14:41:44 +0100

    Add files via upload

---
+++ scripts/sam_to_bam.sh
@@ -0,0 +1,14 @@
+## script to use the alignment files to determine unmapped reads then extract these in fastq formats
+# using samtools
+
+# pt 1 (I'll do this in multiple parts then combine when it works)
+
+
+
+for sample in $(cat ./sample_list.txt)
+do
+	samtools view \
+    -bS \
+    ./unmapped_reads/"$sample"_aln.pe.sam > ./unmapped_reads/"$sample"_aln.pe.bam
+        
+done
