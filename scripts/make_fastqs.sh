e36fe62575daacda244c09df2da45ff358c2d5bc
Laura Brettell <38452231+laura-brettell@users.noreply.github.com>
2022-10-27 16:07:03 +0100

    Update make_fastqs.sh

--- scripts/make_fastqs.sh
+++ scripts/make_fastqs.sh
@@ -7,7 +7,7 @@
 for sample in $(cat ./temp_sample_list.txt)
 do
 	bamToFastq \
-    -i ../unmapped_reads/"$sample"_both_ends_unmapped_sorted.bam  \
+    -i ./unmapped_reads/"$sample"_both_ends_unmapped_sorted.bam  \
     -fq ./unmapped_reads/"$sample"_host_removed_R1.fastq \
     -fq2 ./unmapped_reads/ "$sample"_host_removed_R2.fastq
     
